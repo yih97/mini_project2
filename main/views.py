e98584b1972f25e24676b6796fac6959f47c0c5b
khb3254 <khb3254@naver.com>
2023-09-25 12:42:04 +0900

    지도 데이터 활용
    -models.py : TheNumberOfStudentsPerTeacher 데이터 생성
    - templates:
    메인화면 notion 링크버튼 생성
    - views.py:
    가설 2-1 지도 함수 생성

--- main/views.py
+++ main/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from .models import ClosedSchoolMap
+import folium
 
 def main_page(request):
-    return render(request, 'main/main.html')
+    closed_school_map_df = ClosedSchoolMap.objects.all()
+    map = folium.Map(location=[37.55, 126.98], zoom_start=12)
+    for idx, row in closed_school_map_df.iterrows():  # 데이터프레임 자료를 한 라인씩 정보를 읽는다.
+        folium.Marker([row["Latitude"], row["Longitude"]],
+                      popup=idx).add_to(map)
+    return render(request, 'main/main.html',
+    {"closed_school_map":"closed_school_map"})
 
