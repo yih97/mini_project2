384ac8553179db37a5142fec4488d347b00b525b
khb3254 <khb3254@naver.com>
2023-09-26 09:09:22 +0900

    사진삭제
    css 불필요한 사진 삭제

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+# views.py 시각화 코드
 from django.shortcuts import render
 from django.http import HttpResponse
 import numpy as np
@@ -9,12 +10,38 @@ from folium.plugins import MarkerCluster
 import geocoder
 
 def main_page(request):
-    #closed_school_map_df = ClosedSchoolMap.objects.all()
-    map = folium.Map(location=[37.55, 126.98], zoom_start=12)
-    closed_school_map_df = pd.DataFrame(ClosedSchoolMap.objects.all().values())
-    for idx, row in closed_school_map_df.iterrows():  # 데이터프레임 자료를 한 라인씩 정보를 읽는다.
-        folium.Marker([row["latitude"], row["longitude"]],
-        #Marker([row["Latitude"], row["Longitude"]],
-        popup=idx).add_to(map)
-    return render(request, 'main/main.html',
-    {"map":"map"})
+    figure = folium.Map(location=[37.55, 126.98], zoom_start=12)
+    marker_cluster_kor_marker = MarkerCluster().add_to(figure)
+    popups = []
+
+    for i in ClosedSchoolMap.objects.values():
+        name = []
+        year = []
+
+        name.append(i["school_name"])
+        year.append(i["closed_school_year"])
+
+        popup_html = f"<b>{name[0]} ({year[0]}년 폐교)</b>"  # 팝업 내용 수정
+        popups.append(folium.Popup(popup_html, max_width=200))
+
+    for i in range(len(ClosedSchoolMap.objects.values())):
+        lat = []
+        log = []
+
+        lat.append(ClosedSchoolMap.objects.values()[i]["latitude"])
+        log.append(ClosedSchoolMap.objects.values()[i]["longitude"])
+
+        folium.Marker(
+            location=[lat[0], log[0]],
+            icon=folium.Icon(color='blue'),  # 아이콘 색상 설정
+            popup=popups[i] # 팝업 설정
+        ).add_to(marker_cluster_kor_marker)
+
+    context = {
+        'map': figure._repr_html_()
+           }
+
+    return render(request, 'main/main.html', context)
+
+
+
