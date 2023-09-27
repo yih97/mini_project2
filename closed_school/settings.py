5dd401e1aea71cd69c8458a23a6a61983617f158
BangBnag22 <122610370+yih97@users.noreply.github.com>
2023-09-27 12:37:00 +0900

    찐최종찐진짜로

--- closed_school/settings.py
+++ closed_school/settings.py
@@ -110,9 +110,9 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "closed_school",
         "USER": "root",
-        "PASSWORD": "0000",
+        "PASSWORD": "jong1472!!",
         "HOST": "127.0.0.1",
-        "PORT": "3307",
+        "PORT": "3306",
     }
 }
 
